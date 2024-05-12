f3726234c52c0aa99c62e3488e4b55ec2ca107b4
Simon Kelly <simonkellly@gmail.com>
2024-05-12 18:37:49 +0100

    Add css to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import typography from '@tailwindcss/typography'
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
     extend: {},
