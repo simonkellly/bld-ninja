eee3934e019aac10ee2d3bc2c1cfb635da665739
Simon Kelly <simonkellly@gmail.com>
2024-05-13 01:55:28 +0100

    Add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import bldNinjaLogo from '/bldninja-logo-v1.svg';
 
 function App() {
   return (
-    <article className="prose lg:prose-xl prose-invert">
-      <img src={bldNinjaLogo} alt="BLD Ninja logo" />
-      <h1>BLD Ninja</h1>
-      <p>
-        Work in progress.
-      </p>
-    </article>
+    <main className="flex h-[100dvh] w-full items-center justify-center bg-white dark:bg-gray-950">
+      <div className="flex flex-col items-center gap-6">
+        <img
+          src={bldNinjaLogo} alt="BLD Ninja logo"
+          className="w-96"
+        />
+        <h1 className="text-4xl font-bold tracking-tighter text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
+          Under Construction
+        </h1>
+        <p className="max-w-[400px] text-center text-gray-500 dark:text-gray-400 md:text-lg">
+          We're hard at work, building something amazing for you. Please check back soon for our grand opening!
+        </p>
+      </div>
+    </main>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
   plugins: [react()],
-  base: '/bld-ninja/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
