e692db67f853cfbb2e885ae4c249c5add3dd5f43
Simon Kelly <simonkellly@gmail.com>
2024-05-13 03:18:20 +0100

    Setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,17 @@ function App() {
   return (
     <main className="flex h-[100dvh] w-full items-center justify-center bg-white dark:bg-gray-950">
       <div className="flex flex-col items-center gap-6">
-        <img
-          src={bldNinjaLogo} alt="BLD Ninja logo"
-          className="w-96"
-        />
+        <img src={bldNinjaLogo} alt="BLD Ninja logo" className="w-96" />
         <h1 className="text-4xl font-bold tracking-tighter text-gray-900 dark:text-gray-50 sm:text-5xl md:text-6xl">
           Under Construction
         </h1>
         <p className="max-w-[400px] text-center text-gray-500 dark:text-gray-400 md:text-lg">
-          We're hard at work, building something amazing for you. Please check back soon for our grand opening!
+          We're hard at work, building something amazing for you. Please check
+          back soon for our grand opening!
         </p>
       </div>
     </main>
   );
 }
 
-export default App
+export default App;
