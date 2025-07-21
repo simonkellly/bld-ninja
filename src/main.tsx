8dbc695a36154f7114309530a0beeffdd0e29322
Simon Kelly <simonkellly@gmail.com>
2025-07-22 00:40:39 +0100

    Switch to hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createHashHistory, createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from '@/routeTree.gen'
 import "@/index.css";
 
-const router = createRouter({ routeTree })
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory });
 
 declare module '@tanstack/react-router' {
   interface Register {
